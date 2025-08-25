9b6b485bcddd67103beff483198963c520fcc2c4
Savio <savio13foxy@gmail.com>
2025-08-24 21:10:03 -0300

    Repository

--- src/main/java/com/SpringTESTE/SpringTESTE/Repository/Repository.java
+++ src/main/java/com/SpringTESTE/SpringTESTE/Repository/Repository.java
@@ -1,5 +1,9 @@
 package com.SpringTESTE.SpringTESTE.Repository;
 
-public interface Repository {
+import com.SpringTESTE.SpringTESTE.Entity.Entidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<Entidade, Long> {
+
 
 }
