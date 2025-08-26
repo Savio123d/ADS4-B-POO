8ab077cd38b1a0dc450032396553c985c0fcaf38
Savio123d <savio13foxy@gmail.com>
2025-08-25 21:49:01 -0300

    Controller

--- src/main/java/com/SpringTESTE/SpringTESTE/Controller/Controller.java
+++ src/main/java/com/SpringTESTE/SpringTESTE/Controller/Controller.java
@@ -1,4 +1,42 @@
 package com.SpringTESTE.SpringTESTE.Controller;
 
+import com.SpringTESTE.SpringTESTE.Entity.Entidade;
+import com.SpringTESTE.SpringTESTE.Repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/Entidade")
 public class Controller {
+
+    @Autowired
+    private Repository repo;
+
+    @PostMapping
+    public Entidade criarEntidade(@RequestBody Entidade entidade){
+        return repo.save(entidade);
+    }
+
+    @GetMapping
+    public List<Entidade> listarEntidades(){
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Entidade buscarFuncionarioPorId(@PathVariable Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Entidade atualizarFuncionario(@PathVariable Long id, @RequestBody Entidade funcionario) {
+        funcionario.setId(id);
+        return repo.save(funcionario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarFuncionario(@PathVariable Long id) {
+        repo.deleteById(id);
+    }
 }
